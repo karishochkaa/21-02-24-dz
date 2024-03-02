09c1e46c82235ddd15204a6632008b45875249b8
karishochkaa <128327228+karishochkaa@users.noreply.github.com>
2024-03-02 11:02:04 +0200

    Add files via upload

---
+++ task-01.js
@@ -0,0 +1,16 @@
+// # Завдання 1
+// Напиши функцію countProps(obj), яка рахує кількість властивостей в об'єкті.
+// Функція повертає число - кількість властивостей.
+
+const countProps = function (obj) {
+    return Object.keys(obj).length
+  };
+  
+  /*
+   * Викличи функції для перевірки працездатності твоєї реалізації.
+   */
+  console.log(countProps({})); // 0
+  
+  console.log(countProps({ name: 'Mango', age: 2 })); // 2
+  
+  console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
---
+++ task-02.js
@@ -0,0 +1,46 @@
+// # Завдання 2
+// Напиши функцію findBestEmployee(employees), яка приймає об'єкт співробітників
+// і повертає ім'я найпродуктивнішого (який виконав більше всіх задач).
+// Співробітники і кількість виконаних завдань містяться як властивості об'єкта в
+// форматі "ім'я":"кількість задач".
+
+const findBestEmployee = function (employees) {
+    let bestEmployee = "";
+    let bestEmployeeIndex = 0;
+    for(const key in employees){
+        if(employees[key] > bestEmployeeIndex){
+            employees[key] === bestEmployeeIndex
+            bestEmployee = key;
+        }
+    }
+    return bestEmployee
+  };
+  
+  /*
+   * Викличи функції для перевірки працездатності твоєї реалізації.
+   */
+  console.log(
+    findBestEmployee({
+      ann: 29,
+      david: 35,
+      helen: 1,
+      lorence: 99,
+    }),
+  ); // lorence
+  
+  console.log(
+    findBestEmployee({
+      poly: 12,
+      mango: 17,
+      ajax: 4,
+    }),
+  ); // mango
+  
+  console.log(
+    findBestEmployee({
+      lux: 147,
+      david: 21,
+      kiwi: 19,
+      chelsy: 38,
+    }),
+  ); // lux
